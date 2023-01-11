21b4b57fc1e90a845d3d15c0cafaf5037cf08599
Abhay <as1240143@gmail.com>
2023-01-11 17:51:49 +0530

    .

--- Types of pattern.cpp
+++ Types of pattern.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main(){
+	
+//	Draw Rectangle Pattern.
+	
 	int row,col;
 	cout<<"To Draw Rectangle Pattern\n";
 	cout<<"Enter no. of row:";
@@ -13,6 +16,27 @@ int main(){
 			cout<<"*";
 		}
 		cout<<"\n";
+	}
+	
+//	Draw Hollow Rectangle Pattern
+	
+//	int row,col;
+	cout<<"To Draw Hollow Rectangle Pattern\n";
+	cout<<"Enter no. of row:";
+	cin>>row;
+	cout<<"Enter no. of Columns:";
+	cin>>col;
+	for(int i=1;i<=row;i++){
+		for(int j=1;j<=col;j++){
+			if(i==1 || i==row){
+			cout<<"*";
+		} else if(j==1 || j==col){
+			cout<<"*";
+		}else{
+			cout<<" ";
+		}	
+		}
+		cout<<"\n";	
 	}	
 }
 
