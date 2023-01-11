77f5de364c8cbe119a24439d8957ecee6aed277f
Abhay <as1240143@gmail.com>
2023-01-11 17:55:07 +0530

    .

--- Types of pattern.cpp
+++ Types of pattern.cpp
@@ -68,6 +68,21 @@ int main(){
 		}
 		cout<<"\n";
 	}
+	
+//	Draw Floud's Triangle Pattern
+	
+//	int n;
+	int count=1;
+	cout<<"To Draw Floud's Triangle Pattern\n";
+	cout<<"Enter no. of row:";
+	cin>>n;	
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			cout<<count;
+			count++;
+		}
+		cout<<"\n";
+	}
 }
 
 
